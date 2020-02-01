5ff35b3e81279a2937b14a068ef4cd94af68d571
GideonBrimleaf <chrisdryden1@gmail.com>
2020-02-01 23:33:21 +0000

    working on save method

--- spec/athletes_spec.rb
+++ spec/athletes_spec.rb
@@ -1,7 +1,7 @@
 
 # frozen_string_literal: true
 
-require_relative '../app/models/athletes.rb'
+require_relative('../app/models/athletes.rb')
 
 RSpec.describe Athlete do
 
@@ -16,6 +16,13 @@ RSpec.describe Athlete do
       expect(new_athlete.country).to eql('Scotland')
       expect(new_athlete.hometown).to eql('Edinburgh')
       expect(new_athlete.sponsor).to eql('Burton')
+    end
+  end
+
+  describe '#save' do
+
+    it 'saves Gideon athlete to db' do
+      expect(new_athlete.save).to eql(1)
     end
   end
 
